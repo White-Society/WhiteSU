bd372305bad126eadd665f9f1b0680508496a1cf
MrGadget84 <superhyper52@gmail.com>
2026-09-21 23:43:11 +0400

    malware removal :)

--- userspace/ksud/src/main.rs
+++ userspace/ksud/src/main.rs
@@ -38,8 +38,8 @@ mod module;
 mod module_config;
 #[cfg(target_os = "android")]
 mod profile;
-#[cfg(target_os = "android")]
-mod risk;
+// #[cfg(target_os = "android")]
+// mod risk;
 #[cfg(target_os = "android")]
 mod resetprop;
 #[cfg(target_os = "android")]
