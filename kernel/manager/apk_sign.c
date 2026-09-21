27d76c3aa8f68861acd60888849dce641722b77f
MrGadget84 <superhyper52@gmail.com>
2026-09-22 00:42:40 +0400

    idk btw

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -344,16 +344,15 @@ int get_pkg_from_apk_path(char *pkg, const char *path)
 bool is_manager_apk(char *path)
 {
 #ifdef KSU_MANAGER_PACKAGE
-	char pkg[KSU_MAX_PACKAGE_NAME];
-	if (get_pkg_from_apk_path(pkg, path) < 0) {
-		pr_err("Failed to get package name from apk path: %s\n", path);
-		return false;
-	}
-
-	// pkg is `<real package>`
-	if (strncmp(pkg, KSU_MANAGER_PACKAGE, sizeof(KSU_MANAGER_PACKAGE))) {
-		return false;
-	}
+    char pkg[KSU_MAX_PACKAGE_NAME];
+    if (get_pkg_from_apk_path(pkg, path) < 0) {
+        pr_err("Failed to get package name from apk path: %s\n", path);
+        return false;
+    }
+    if (strncmp(pkg, KSU_MANAGER_PACKAGE, sizeof(KSU_MANAGER_PACKAGE))) {
+        return false;
+    }
 #endif
-	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+    u8 local_sig_idx = 0;
+    return check_v2_signature(path, &local_sig_idx);
 }
